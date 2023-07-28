97124712aa943dbd257575c65b52a2b578eafb1d
woodsnip <woodsnib@ufl.edu>
2023-07-28 15:05:03 -0400

    added url validation to links form

---
+++ src/app/api/links/route.js
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+import isValidURL from "@/app/lib/isValidURL";
+
+export async function POST(request) {
+
+    const contentType = await request.headers.get("content-type")
+    if (contentType !== "application/json") {
+        return NextResponse.json({"error": "Invalid request"}, 
+        {status: 415})
+    }
+    const data = await request.json()
+    const url = data && data.url ? data.url : null
+    const validURL = await isValidURL(url, ["link-shortener", process.env.NEXT_PUBLIC_VERCEL_URL])
+    if (!validURL) {
+        return NextResponse.json({"message": `${url} is not valid.`}, 
+        {status: 400})
+    }
+    return NextResponse.json(data, {status: 201})
+
+}
---
+++ src/app/links/createForm.js
@@ -0,0 +1,37 @@
+'use client'
+
+import { useState } from "react"
+
+export default function LinksCreateForm () {
+    const [results, setResults] = useState(null)
+
+    const handleForm = async (event) => {
+        event.preventDefault()
+        
+        const formData = new FormData(event.target)
+        const data = Object.fromEntries(formData)
+        const JSONData = JSON.stringify(data)
+        const endpoint = "/api/links/"
+        const options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSONData
+        }
+        const response = await fetch(endpoint, options)
+
+        const result = await response.json()
+        console.log(result)
+        setResults(result)
+    }
+
+    return <>
+        <form onSubmit={handleForm}>
+            <input type="text" defaultValue="https://github.com/woodsnib/link-shortener" 
+            name="url" placeholder="URL to shorten" />
+            <button type="submit">Shorten</button>
+        </form>
+        {results && JSON.stringify(results)}
+    </>
+}
