9ce51f72cf57205eef57fd8bc808d784da74d157
woodsnip <woodsnib@ufl.edu>
2023-07-27 20:37:01 -0400

    removed fetch

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,13 +1,14 @@
 async function getData() {
-    //1 endpoint - api
-    const endpoint = "http://localhost:3000/api/posts"
-    const res = await fetch(endpoint)
+    // //1 endpoint - api
+    // const endpoint = "http://localhost:3000/api/posts"
+    // const res = await fetch(endpoint)
 
-    if (!res.ok) {
-        throw new Error("Failed to fetch data")
-    }
+    // if (!res.ok) {
+    //     throw new Error("Failed to fetch data")
+    // }
 
-    return res.json()
+    // return res.json()
+    return {}
 }
 
 
